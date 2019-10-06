ac7c7142f6917477b0081891dff0d7f838356083
Ang Sivhour <45730005+sivhourang@users.noreply.github.com>
2019-10-06 23:13:33 +0700

    Update MyHelper.kt

--- app/src/main/java/com/example/app21/MyHelper.kt
+++ app/src/main/java/com/example/app21/MyHelper.kt
@@ -16,11 +16,9 @@ class MyHelper(context: Context): SQLiteOpenHelper(context, DB_NAME, null, 1) {
         var create_table = "create table $TABLE_NAME ($COL1 varchar(250), $COL2 varchar(250))"
         p0?.execSQL(create_table)
     }
-
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        
     }
-
     fun insertData(dto: Staff): Long{
         var cv = ContentValues()
 
@@ -29,10 +27,8 @@ class MyHelper(context: Context): SQLiteOpenHelper(context, DB_NAME, null, 1) {
 
         var db = this.writableDatabase
         var res = db.insert(TABLE_NAME, null, cv)
-
         return res
     }
-
     fun readRecord(): ArrayList<Staff>{
         var list = ArrayList<Staff>()
         var db = this.readableDatabase
